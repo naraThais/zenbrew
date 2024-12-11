6d24b41bbfc1831827c2f63e943e408946eb537b
Thais Nara <thaisna46@gmail.com>
2024-12-11 18:12:10 -0300

    Porject Setup

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+import Hero from "@/components/Hero";
+const Home = () => {
+  return (
+    <div className="h-full overflow-x-hidden"><Hero/></div>
+  )
+}
+
+export default Home
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,37 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container:{
+      center: true,
+      padding:"15px",
+    },
+    screens:{
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1430px",
+    },
+    fontFamily:{
+      primery: "var(--font-cormorant_upright)",
+      secondary: "var(--font-open_sans)",
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#100e0e",
+        },
+        secondary:{
+          DEFAULT: "#787f8a",
+        },
+        accent:{
+          DEFAULT: "#c7a17a",
+          hover: "#a08161",
+        },
+      },
+      backgroundImage:{
+        hero_overlay: "url('/assets/hero/hero-overlay.png')",
+        opening_hours: "url('/assets/hero/opening-hours/bg.png')",
+        footer: "url('/assets/footer/bg.png)",
       },
     },
   },
