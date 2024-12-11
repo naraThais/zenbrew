bf9520f055001a772c2bc073d5658f412f363c71
Thais Nara <thaisna46@gmail.com>
2024-12-11 19:28:12 -0300

    Primeira etapa Hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,44 +6,6 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    container:{
-      center: true,
-      padding:"15px",
-    },
-    screens:{
-      sm: "640px",
-      md: "768px",
-      lg: "960px",
-      xl: "1430px",
-    },
-    fontFamily:{
-      primary: "var(--font-cormorant_upright)",
-      secondary: "var(--font-open_sans)",
-    },
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: "#100e0e",
-        },
-        secondary:{
-          DEFAULT: "#787f8a",
-        },
-        accent:{
-          DEFAULT: "#c7a17a",
-          hover: "#a08161",
-        },
-      },
-      backgroundImage:{
-        hero_overlay: "url('/assets/hero/hero-overlay.png')",
-        opening_hours: "url('/assets/hero/opening-hours/bg.png')",
-        footer: "url('/assets/footer/bg.png)",
-      },
-    },
-  },
-  plugins: [],
-};
-module.exports = {
-  theme: {
     extend: {
       fontFamily: {
         primary: 'var(--font-cormorant_upright)', // Variável para Cormorant Upright
