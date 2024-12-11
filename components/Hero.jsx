6679eb1caa6bd3ffc52072b2ce0379ff5dda649c
Thais Nara <thaisna46@gmail.com>
2024-12-11 18:39:21 -0300

    Hero MP4 Page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,13 +1,20 @@
 "use client";
 
-
-
-import React from 'react'
+import Separator from "./Separator";
+import Badge from "./Badge";
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="h-[80vh] xl:h-screen relative text-white ">
+      {/* overlay */}
+      <div>overlay</div>
+      {/* video */}
+      <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover">
+        <source src="/assets/hero/video.mp4" type="video/mp4" />
+      </video>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,7 +17,7 @@ export default {
       xl: "1430px",
     },
     fontFamily:{
-      primery: "var(--font-cormorant_upright)",
+      primary: "var(--font-cormorant_upright)",
       secondary: "var(--font-open_sans)",
     },
     extend: {
@@ -40,5 +40,38 @@ export default {
       },
     },
   },
+  plugins: [],
+};
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: 'var(--font-cormorant_upright)', // Variável para Cormorant Upright
+        secondary: 'var(--font-open_sans)',      // Variável para Open Sans
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#100e0e",
+        },
+        secondary: {
+          DEFAULT: "#787f8a",
+        },
+        accent: {
+          DEFAULT: "#c7a17a",
+          hover: "#a08161",
+        },
+      },
+      backgroundImage: {
+        hero_overlay: "url('/assets/hero/hero-overlay.png')",
+        opening_hours: "url('/assets/hero/opening-hours/bg.png')",
+        footer: "url('/assets/footer/bg.png')",
+      },
+    },
+  },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   plugins: [],
 };
